b8171200c93c1c1b7fa102a29bf26a8f861e9a56
R1mok <anton150801@gmail.com>
2020-05-12 13:12:33 +0500

    Исправление функции d_find_by_key

--- lab5.c
+++ lab5.c
@@ -266,9 +266,9 @@ void d_find_by_key(Node* root) // диалоговая функция поиск
 	else
 	{
 		printf("Key: %s Info: %s", cur->key, cur->info);
-		free(cur->info);
-		free(cur->key);
-		free(cur);
+		//free(cur->info);
+		//free(cur->key);
+		//free(cur);
 	}
 	free(key);
 }
